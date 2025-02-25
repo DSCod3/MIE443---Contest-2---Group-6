9c1d8e345caba6e3e41f347ee025456f2cba31d5
DSCod3 <dhruvsharmaone@gmail.com>
2025-02-25 16:19:53 -0500

    Added surf stuff and async spinner
    On branch dhruv_testing
    Your branch is up to date with 'origin/dhruv_testing'.
    
    Changes to be committed:
    	modified:   include/imagePipeline.h
    	modified:   src/contest2.cpp
    	modified:   src/imagePipeline.cpp

--- include/imagePipeline.h
+++ include/imagePipeline.h
@@ -1,19 +1,33 @@
-#pragma once
+#ifndef IMAGEPIPELINE_H
+#define IMAGEPIPELINE_H
 
+#include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <std_msgs/String.h>
-#include <opencv2/core.hpp>
-#include <cv.h>
 #include <cv_bridge/cv_bridge.h>
-#include <boxes.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/core.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/features2d.hpp>
+
+class Boxes; // Forward declaration to avoid circular dependency.
 
 class ImagePipeline {
-    private:
-        cv::Mat img;
-        bool isValid;
-        image_transport::Subscriber sub;
-    public:
-        ImagePipeline(ros::NodeHandle& n);
-        void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-        int getTemplateID(Boxes& boxes);
+public:
+    ImagePipeline(ros::NodeHandle& n);
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+    // Uses SURF to detect a tag from the current image against the provided templates.
+
+    // Returns the index of the best matching template (or -1 if no good match is found).
+    int getTemplateID(Boxes& boxes);
+    
+    bool isValid;
+    cv::Mat img;
+    
+private:
+    image_transport::Subscriber sub;
+    cv::Ptr<cv::xfeatures2d::SURF> detector;
+    cv::Ptr<cv::BFMatcher> matcher;
 };
+
+#endif
