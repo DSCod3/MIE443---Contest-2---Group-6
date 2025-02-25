0f6bbd0175b8e98fdd9940f57fd62bf3750e096a
DSCod3 <dhruvsharmaone@gmail.com>
2025-02-25 18:55:23 -0500

    Offset stuff updated and yaw inverted
    On branch dhruv_testing
    Your branch is up to date with 'origin/dhruv_testing'.
    
    Changes to be committed:
    	modified:   src/contest2.cpp
    	modified:   src/navigation.cpp

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -2,34 +2,31 @@
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <tf/transform_datatypes.h>
+#include <ros/ros.h>
 
 bool Navigation::moveToGoal(float xGoal, float yGoal, float phiGoal){
-	// Set up and wait for actionClient.
+    // Set up and wait for the action client.
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
     while(!ac.waitForServer(ros::Duration(5.0))){
         ROS_INFO("Waiting for the move_base action server to come up");
     }
-	// Set goal.
-    geometry_msgs::Quaternion phi = tf::createQuaternionMsgFromYaw(phiGoal);
+    // Convert phi from degrees to radians.
+    geometry_msgs::Quaternion phi = tf::createQuaternionMsgFromYaw(-phiGoal * M_PI / 180.0);
     move_base_msgs::MoveBaseGoal goal;
     goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x =  xGoal;
-    goal.target_pose.pose.position.y =  yGoal;
-    goal.target_pose.pose.position.z =  0.0;
-    goal.target_pose.pose.orientation.x = 0.0;
-    goal.target_pose.pose.orientation.y = 0.0;
-    goal.target_pose.pose.orientation.z = phi.z;
-    goal.target_pose.pose.orientation.w = phi.w;
-    ROS_INFO("Sending goal location ...");
-	// Send goal and wait for response.
+    goal.target_pose.pose.position.x = xGoal;
+    goal.target_pose.pose.position.y = yGoal;
+    goal.target_pose.pose.position.z = 0.0;
+    goal.target_pose.pose.orientation = phi;
+    ROS_INFO("Sending goal: (%.2f, %.2f, %.2f)", xGoal, yGoal, phiGoal);
     ac.sendGoal(goal);
     ac.waitForResult();
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-        ROS_INFO("You have reached the destination");
+        ROS_INFO("Goal reached successfully.");
         return true;
     } else {
-        ROS_INFO("The robot failed to reach the destination");
+        ROS_INFO("Failed to reach goal: %s", ac.getState().toString().c_str());
         return false;
     }
 }
